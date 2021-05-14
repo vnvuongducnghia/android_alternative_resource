69def6de3d50bec94aef259ccd61c71ee41a8c0a
nghia.vuong <nghia.vuong@vn.defide.co.jp>
2021-05-14 17:02:24 +0700

    create project

---
+++ AndroidAlternativeResource/app/src/main/java/com/nghia/androidalternativeresource/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nghia.androidalternativeresource
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
